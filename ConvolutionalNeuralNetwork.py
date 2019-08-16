7cc154c8b866c0295b866b4498f7f5f88c721507
btknzn <48128295+btknzn@users.noreply.github.com>
2019-08-16 10:41:09 +0200

    Add files via upload

---
+++ ConvolutionalNeuralNetwork.py
@@ -0,0 +1,47 @@
+#Convolutional Neural Network
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+classifier = Sequential()
+#Step 1 - Convolution
+classifier.add(Convolution2D(32,3,3,input_shape=(64,64,3),activation = 'relu'))
+#Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+#increase the success of our sysyem
+classifier.add(Convolution2D(32,3,3,input_shape=(64,64,3),activation = 'relu'))
+#Step3 - Flattening
+classifier.add(Flatten())
+#Full Connection step
+classifier.add(Dense(output_dim=128, activation ='relu'))
+classifier.add(Dense(output_dim=128, activation ='sigmoid'))
+
+#Compiling the CNN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+#Part 2 -Fitting the CNN
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 2000)
